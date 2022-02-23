9923792837b22d3293e9e5d4929e7a19cd576951
hemapriya <hemapriyasampath@gmail.com>
2022-02-23 11:07:57 -0500

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <span>
+        <h3>Example of Context App</h3>
+      </span>
+    </div>
+  );
+};
+
+export default Header;
